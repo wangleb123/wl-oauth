2079259140bd49e1c1e2eaa8da112734875e0f19
王乐 <1660104869@qq.com>
2020-08-13 15:22:40 +0800

    first commit

---
+++ src/main/java/com/wangle/oauth/WlOauthBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.wangle.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlOauthBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WlOauthBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangle/oauth/WlOauthBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangle.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlOauthBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
