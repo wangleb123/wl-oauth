506536118e07cd4ef649cd345a40ea5706752183
王乐 <1660104869@qq.com>
2020-08-13 15:21:36 +0800

    first commit

---
+++ src/test/java/com/lexiang/redis/WlRedisBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexiang.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlRedisBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
